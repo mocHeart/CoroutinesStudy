ee391b6d6158ecdd943bc677776c66022ca3de6a
jianyao.li <nuc2015@163.com>
2025-03-12 21:56:26 +0800

    Demo03:MainScope协程作用域-继承方式

--- app/src/main/java/com/hg/crs/MainActivity.kt
+++ app/src/main/java/com/hg/crs/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.hg.crs.demo01.Demo01Activity
 import com.hg.crs.demo02.Demo02Activity
 import com.hg.crs.demo03.Demo03Activity
+import com.hg.crs.demo04.Demo04Activity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +26,11 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.btn_dem03).setOnClickListener{
             intent = Intent(this, Demo03Activity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.btn_dem04).setOnClickListener{
+            intent = Intent(this, Demo04Activity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/hg/crs/demo04/Demo04Activity.kt
@@ -0,0 +1,49 @@
+package com.hg.crs.demo04
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.hg.crs.R
+import com.hg.crs.demo02.userServiceApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@SuppressLint("MissingInflatedId", "StaticFieldLeak", "SetTextI18n")
+class Demo04Activity : AppCompatActivity(), CoroutineScope by MainScope() {
+
+    private lateinit var nameTv: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo01)
+
+        nameTv = findViewById<TextView>(R.id.demo01_nameTv)
+        nameTv.text = "Guangzhou"
+
+        val submitBtn = findViewById<Button>(R.id.demo01_submitBtn).also {
+            it.setOnClickListener {
+                launch() {
+                    // 在协程作用域里的挂机函数会自动协程支持
+                    val cities = userServiceApi.getCities()
+                    nameTv.text = "id: ${cities?.get(4)?.id} => City: ${cities?.get(4)?.name}"
+                    try {
+                        delay(10000)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // 取消协程
+        cancel()
+    }
+}
