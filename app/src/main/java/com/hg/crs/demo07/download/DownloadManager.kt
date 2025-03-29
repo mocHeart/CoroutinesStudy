da7f93f1bc34692e2a970e9d1e1206cbdf9d0876
jianyao.li <nuc2015@163.com>
2025-03-29 21:02:59 +0800

    Demo07:协程Flow的综合应用-02文件下载

---
+++ app/src/main/java/com/hg/crs/demo07/download/DownloadManager.kt
@@ -0,0 +1,46 @@
+package com.hg.crs.demo07.download
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.File
+import com.hg.crs.demo07.utils.copyTo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import okio.IOException
+
+object DownloadManager {
+    fun download(url: String, file: File): Flow<DownloadStatus> {
+        return flow {
+            val request = Request.Builder().url(url).get().build()
+            val response = OkHttpClient.Builder().build().newCall(request).execute()
+            if (response.isSuccessful) {
+                response.body()!!.let { body ->
+                    val total = body.contentLength()
+                    // 文件读写
+                    file.outputStream().use { output ->
+                        val input = body.byteStream()
+                        var emittedProgress = 0L
+                        input.copyTo(output) { bytesCopied ->
+                            val progress = bytesCopied * 100 / total
+                            if (progress - emittedProgress > 5) {
+                                delay(200)
+                                emit(DownloadStatus.Progress(progress.toInt()))
+                                emittedProgress = progress
+                            }
+                        }
+                    }
+                }
+                emit(DownloadStatus.Done(file))
+            } else {
+                throw IOException(response.toString())
+            }
+        }.catch {
+            file.delete()
+            emit(DownloadStatus.Error(it))
+        }.flowOn(Dispatchers.IO)
+    }
+}
--- app/src/main/java/com/hg/crs/demo07/fragment/D07DownloadFragment.kt
+++ app/src/main/java/com/hg/crs/demo07/fragment/D07DownloadFragment.kt
@@ -1,13 +1,21 @@
 package com.hg.crs.demo07.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.hg.crs.databinding.FragmentD07DownloadBinding
+import com.hg.crs.demo07.download.DownloadManager
+import com.hg.crs.demo07.download.DownloadStatus
+import com.hjq.toast.Toaster
+import java.io.File
 
 class D07DownloadFragment : Fragment() {
+
+    val URL = "http://116.198.231.162:3100/avatars/309b1e19c3824018c46d96da25f1d4652a54b66d0d7aceae1ba20332faed47ce?size=56"
 
     private val mBinding: FragmentD07DownloadBinding by lazy {
         FragmentD07DownloadBinding.inflate(layoutInflater)
@@ -18,5 +26,36 @@ class D07DownloadFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return mBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            context?.apply {
+                val file = File(getExternalFilesDir(null)?.path, "pic.jpg")
+                DownloadManager.download(URL, file).collect { status ->
+                    when (status) {
+                        is DownloadStatus.Progress -> {
+                            mBinding.apply {
+                                progressBar.progress = status.value
+                                tvProgress.text = "${status.value}"
+                            }
+                        }
+                        is DownloadStatus.Error -> {
+                            Toaster.show("下载错误")
+                        }
+                        is DownloadStatus.Done -> {
+                            mBinding.apply {
+                                progressBar.progress = 100
+                                tvProgress.text = "100%"
+                            }
+                        }
+                        else -> {
+                            Log.d("JY", "下载失败")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
